7be7810a7e23c646d72b2582748172e3b7b68511
Michael-elie <michael-elie@hotmail.com>
2025-04-23 16:55:31 +0200

    df

--- Assets/Code/Scripts/Source/Managers/SoundManager.cs
+++ Assets/Code/Scripts/Source/Managers/SoundManager.cs
@@ -1,20 +1,64 @@
-
+using System;
 using Code.Scripts.Utils;
+using UnityEngine;
 
 namespace Code.Scripts.Source.Managers
 {
-    public class SoundManager : MonoBehaviourSingleton<SoundManager>
+    public enum SoundType
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        Pressed,
+        Canceled,
+        Selected,
+   
+    }
+
+    [RequireComponent(typeof(AudioSource)), ExecuteInEditMode]
+    public class SoundManager : MonoBehaviour
+    {
+        [SerializeField] private SoundList[] _soundList;
+        private static SoundManager _instance;
+        private AudioSource _audioSource;
+    
+        private void Awake()
         {
-        
+            _instance = this;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public static void PlaySound(SoundType sound, float volume = 1)
+        {
+            AudioClip[] clips = _instance._soundList[(int)sound].Sounds;
+            AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+            _instance._audioSource.PlayOneShot(randomClip, volume);
+        }
+
+#if UNITY_EDITOR
+
+        private void OnEnable()
+        {
+            string[] names = Enum.GetNames(typeof(SoundType));
+            Array.Resize(ref _soundList, names.Length);
+            for (int i = 0; i < _soundList.Length; i++)
+            {
+                _soundList[i].Name = names[i];
+            }
+        }
+#endif
+
+        [Serializable]
+        public struct SoundList
+        {
+            public AudioClip[] Sounds
+            {
+                get => _sounds;
+            }
+
+            [HideInInspector] public string Name;
+            [SerializeField] private AudioClip[] _sounds;
         }
     }
 }
