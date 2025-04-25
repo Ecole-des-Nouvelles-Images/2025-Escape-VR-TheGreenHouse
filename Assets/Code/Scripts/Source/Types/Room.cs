5c161a8ec48d215268cf256041bd7aac9c448b3c
Charlie BOYER <charlieboyer@outlook.fr>
2025-04-25 15:30:54 +0200

    chores(scripts): Create Room enum and Type folder for future layer management feature

---
+++ Assets/Code/Scripts/Source/Types/Room.cs
@@ -0,0 +1,14 @@
+namespace Code.Scripts.Source.Types
+{
+    public enum Room
+    {
+        Hall,
+        Living,
+        Backyard,
+        Greenhouse,
+        Laboratory,
+        Corridor1,
+        Corridor2,
+        Corridor3
+    }
+}
