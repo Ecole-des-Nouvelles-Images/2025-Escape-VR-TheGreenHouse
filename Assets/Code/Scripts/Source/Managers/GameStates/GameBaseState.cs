d074059b59aad225a630de289be07a61422328cc
Charlie BOYER <charlieboyer@outlook.fr>
2025-04-25 18:49:51 +0200

    feature(gameplay): Introduce FSM pattern for GameManager, with empty files for now
    + Fill-in enumeration of all GameState

---
+++ Assets/Code/Scripts/Source/Managers/GameStates/GameBaseState.cs
@@ -0,0 +1,7 @@
+namespace Code.Scripts.Source.Managers.GameStates
+{
+    public abstract class GameBaseState
+    {
+
+    }
+}
---
+++ Assets/Code/Scripts/Source/Managers/GameStates/GameState.cs
@@ -0,0 +1,15 @@
+namespace Code.Scripts.Source.Managers.GameStates
+{
+    public enum GameState
+    {
+        GameStart,
+        HallIntro, HallInProgress, HallResolved,
+        LoungeIntro, LoungePhase1, LoungePhase2, LoungeResolved,
+        BackyardTransition,
+        GreenhouseIntro, GreenhouseInProgress, GreenhouseResolved,
+        LaboratoryIntro, LaboratoryPhase1, LaboratoryPhase2, LaboratoryResolved,
+        EscapeRun,
+        EscapeTransition,
+        GameOver
+    }
+}
