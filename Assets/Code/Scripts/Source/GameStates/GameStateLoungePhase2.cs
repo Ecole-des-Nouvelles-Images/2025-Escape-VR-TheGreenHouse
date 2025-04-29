4cf86d2621a5689916a744d96fce6d501f767b6d
Michael-elie <michael-elie@hotmail.com>
2025-04-29 11:15:13 +0200

    merge : clean up scripts folders
    # Conflicts:
    #	Assets/Settings/Project Configuration/UniversalRenderPipelineGlobalSettings.asset
    #	ProjectSettings/EditorBuildSettings.asset

--- Assets/Code/Scripts/Source/GameStates/GameStateLoungePhase2.cs
+++ Assets/Code/Scripts/Source/GameStates/GameStateLoungePhase2.cs
@@ -1,16 +1,22 @@
+using System.Collections.Generic;
 using Code.Scripts.Source.Managers;
+using UnityEngine.UIElements;
 
 namespace Code.Scripts.Source.GameStates
 {
     public class GameStateLoungePhase2 : GameBaseState
     {
+       
+        
         public override void EnterState(GameStateManager context)
         {
+            
         }
 
         public override void UpdateState(GameStateManager context)
         {
-
+           
+           //action to check
         }
 
         public override void ExitState(GameStateManager context)
