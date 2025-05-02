70d87e95aa34b2bb3ad0365d8c2a09ad8b7be53a
Charlie Boyer <charlieboyer@outlook.fr>
2025-05-02 22:49:52 +0200

    refactor(main-menu): small changes for listeners correct (un)subscribtion

--- Assets/Code/Scripts/Source/UI/MainMenu.cs
+++ Assets/Code/Scripts/Source/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Code.Scripts.Source.Managers;
 using UnityEngine;
@@ -12,61 +13,69 @@ namespace Code.Scripts.Source.UI
         [SerializeField] private GameObject _optionsPanel;
         [SerializeField] private GameObject _creditsPanel;
 
-        [Header("Main Menu Buttons")] 
+        [Header("Main Menu Buttons")]
         [SerializeField] private Button _startButton;
         [SerializeField] private Button _optionsButton;
         [SerializeField] private Button _creditsButton;
         [SerializeField] private Button _quitButton;
-   
-        public List<Button> _returnButtons;
 
-        private void Start() 
+        [SerializeField] private List<Button> _returnButtons;
+
+        private void OnEnable()
         {
             _startButton.onClick.AddListener(StartGame);
             _optionsButton.onClick.AddListener(EnableOptionsPanel);
             _creditsButton.onClick.AddListener(EnableCreditsPanel);
             _quitButton.onClick.AddListener(QuitGame);
-       
-       
+
             foreach (Button item in _returnButtons)
-            {
                 item.onClick.AddListener(EnableMainMenuPanel);
-            }
         }
-   
-        private void StartGame() 
+
+        private void OnDisable()
+        {
+            _startButton.onClick.RemoveListener(StartGame);
+            _optionsButton.onClick.RemoveListener(EnableOptionsPanel);
+            _creditsButton.onClick.RemoveListener(EnableCreditsPanel);
+            _quitButton.onClick.RemoveListener(QuitGame);
+
+            foreach (Button item in _returnButtons)
+                item.onClick.RemoveListener(EnableMainMenuPanel);
+        }
+
+        private void StartGame()
         {
             HideAllPanels();
             SceneTransitionManager.Instance.LoadScene("2 Game");
         }
-   
-        private void QuitGame() 
+
+        private void QuitGame()
         {
             Application.Quit();
         }
 
-        private void HideAllPanels() 
+        private void HideAllPanels()
         {
             _mainMenuPanel.SetActive(false);
             _optionsPanel.SetActive(false);
             _creditsPanel.SetActive(false);
         }
-   
-        private void EnableMainMenuPanel() 
+
+        private void EnableMainMenuPanel()
         {
             _mainMenuPanel.SetActive(true);
             _optionsPanel.SetActive(false);
             _creditsPanel.SetActive(false);
         }
-   
-        private void EnableOptionsPanel() 
+
+        private void EnableOptionsPanel()
         {
             _mainMenuPanel.SetActive(false);
             _optionsPanel.SetActive(true);
             _creditsPanel.SetActive(false);
         }
-   
-        private void EnableCreditsPanel() 
+
+        private void EnableCreditsPanel()
         {
             _mainMenuPanel.SetActive(false);
             _optionsPanel.SetActive(false);
