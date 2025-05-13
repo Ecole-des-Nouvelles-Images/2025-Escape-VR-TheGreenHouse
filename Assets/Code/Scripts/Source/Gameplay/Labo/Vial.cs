9a92b3b8c52b9be11a6210d322b0aebe4a9c15c0
Noah MILIANI <120110545+NoahMil@users.noreply.github.com>
2025-05-13 17:21:33 +0200

    feature(laboratory) : start puzzle

---
+++ Assets/Code/Scripts/Source/Gameplay/Labo/Vial.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.Scripts.Source.Gameplay.Labo
+{
+    public class Vial : MonoBehaviour
+    {
+        enum PotionType
+        {
+            Tulénium,
+            Yzora,
+            Morazium,
+            Veridox,
+            Agent,
+            Base
+        }
+        
+        [SerializeField] private PotionType _potionType;
+        public float correctQuantitative
+        {
+            get
+            {
+                switch (_potionType)
+                {
+                    case PotionType.Tulénium :
+                        return 0.25f;
+                    case PotionType.Yzora :
+                        return 0.75f;
+                    case PotionType.Morazium :
+                        return 0.50f;
+                    case PotionType.Veridox :
+                        return 1f;
+                    case PotionType.Agent :
+                        return 0.5f;
+                    case PotionType.Base :
+                        return 1f;
+                    default: return 0f;
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/Code/Scripts/Source/Gameplay/Labo/VialMixer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Code.Scripts.Source.Gameplay.Labo
+{
+    public class VialMixer : MonoBehaviour
+    {
+        private Vial _vialToMix1;
+        private Vial _vialToMix2;
+        
+        
+    }
+}
